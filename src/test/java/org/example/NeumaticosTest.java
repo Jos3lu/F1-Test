ca0f794e050773f9941ed15a05d77765493ef7d2
Jos3lu <jlsierrabenito@gmail.com>
2023-09-29 18:16:52 +0200

    Minor changes

--- src/test/java/org/example/NeumaticosTest.java
+++ src/test/java/org/example/NeumaticosTest.java
@@ -5,9 +5,6 @@ import org.example.neumaticos.neumatico.Neumatico;
 import org.example.neumaticos.neumatico.exceptions.PorcentajeDeVidaNoValidoException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.List;
